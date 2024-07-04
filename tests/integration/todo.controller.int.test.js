8f383e00aa165666eac22667df191e8ce2f6d3d9
Norton Almeida <norton.almeida@gmail.com>
2024-07-04 15:25:42 -0300

    feat: added unit and integration tests for DELETE http method.

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -1,33 +1,33 @@
-const request = require('supertest');
-const app = require('../../app');
-const newTodo = require('../mock-data/new-todo.json');
+const request = require("supertest");
+const app = require("../../app");
+const newTodo = require("../mock-data/new-todo.json");
 
-const endpointUrl = '/todos/';
+const endpointUrl = "/todos/";
 
 let firstTodo, newTodoId;
+const nonExistingTodoId = "5d5fff416bef3c07ecf11f77";
+const testData = { title: "Make integration test for PUT", done: true };
 
 describe(endpointUrl, () => {
-  it("GET " + endpointUrl, async () => {
+  test("GET " + endpointUrl, async () => {
     const response = await request(app).get(endpointUrl);
+
     expect(response.statusCode).toBe(200);
-    expect(Array.isArray).toBeTruthy();
+    expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body[0].title).toBeDefined();
     expect(response.body[0].done).toBeDefined();
     firstTodo = response.body[0];
   });
-
-  it("GET by Id " + endpointUrl + ":todoId", async () => {
+  test("GET by Id " + endpointUrl + ":todoId", async () => {
     const response = await request(app).get(endpointUrl + firstTodo._id);
     expect(response.statusCode).toBe(200);
     expect(response.body.title).toBe(firstTodo.title);
     expect(response.body.done).toBe(firstTodo.done);
   });
-
-  it("GET todo by id doesn't exist" + endpointUrl + ":todoId", async() => {
-    const response = await request(app).get(endpointUrl + "6686be19224fc1bbe5da7ec4");
+  test("GET todoby id doesn't exist" + endpointUrl + ":todoId", async () => {
+    const response = await request(app).get(endpointUrl + nonExistingTodoId);
     expect(response.statusCode).toBe(404);
   });
-
   it("POST " + endpointUrl, async () => {
     const response = await request(app)
       .post(endpointUrl)
@@ -37,22 +37,44 @@ describe(endpointUrl, () => {
     expect(response.body.done).toBe(newTodo.done);
     newTodoId = response.body._id;
   });
-
-  it("should return error 500 on malformed data with POST " + endpointUrl, async () => {
-    const response = await request(app)
-      .post(endpointUrl)
-      .send({ title: "Missing done property" });
-    expect(response.statusCode).toBe(500);
-    expect(response.body).toStrictEqual({
-      "message": "Todo validation failed: done: Path `done` is required."
-    });
-  });
-
+  it(
+    "should return error 500 on malformed data with POST" + endpointUrl,
+    async () => {
+      const response = await request(app)
+        .post(endpointUrl)
+        .send({ title: "Missing done property" });
+      expect(response.statusCode).toBe(500);
+      expect(response.body).toStrictEqual({
+        message: "Todo validation failed: done: Path `done` is required."
+      });
+    }
+  );
   it("PUT " + endpointUrl, async () => {
-    const testData = { title: "Updated title", done: true }
-    const response = await request(app).put(endpointUrl + newTodoId).send(testData);
-    expect(response.statusCode).toBe(200);
-    expect(response.body.title).toBe(testData.title);
-    expect(response.body.done).toBe(testData.done);
-  }) 
+    const res = await request(app)
+      .put(endpointUrl + newTodoId)
+      .send(testData);
+    expect(res.statusCode).toBe(200);
+    expect(res.body.title).toBe(testData.title);
+    expect(res.body.done).toBe(testData.done);
+  });
+  it("should return 404 on PUT " + endpointUrl, async () => {
+    const res = await request(app)
+      .put(endpointUrl + nonExistingTodoId)
+      .send(testData);
+    expect(res.statusCode).toBe(404);
+  });
+  test("HTTP DELETE", async () => {
+    const res = await request(app)
+      .delete(endpointUrl + newTodoId)
+      .send();
+    expect(res.statusCode).toBe(200);
+    expect(res.body.title).toBe(testData.title);
+    expect(res.body.done).toBe(testData.done);
+  });
+  test("HTTP DELETE 404", async () => {
+    const res = await request(app)
+      .delete(endpointUrl + nonExistingTodoId)
+      .send();
+    expect(res.statusCode).toBe(404);
+  });
 });
